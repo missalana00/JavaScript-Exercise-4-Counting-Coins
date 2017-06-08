0c66834f8a6e62d212b1eae98b0ef250646e1203
alana.rae.smith@gmail.com <alana.rae.smith@gmail.com>
2017-06-08 09:38:09 -0500

    Added html so user can enter their dollar amount in a text field and click a button thatcalculates how many of each kind of coin there are in the amount. Working on creating a for in loop to iterate through the coin purse and create the html for users to view results on browser.

--- coins.js
+++ coins.js
@@ -15,33 +15,80 @@
 */
 
 
-function coinCounter (x) {
+// NECESSARY REFERENCES
+
+  // Add the button with an ID of "calcButton" to the variable calculateButton
+  var calculateButton = document.getElementById("calcButton");
+  // Add input with an ID of 'amtToCalc' to the variable userInput
+  var userInput = document.getElementById("amtToCalc");
+  // Add div with ID of 'results' to the variable resultsDiv
+  var resultDiv = document.getElementById("results");
+
+
+// COIN-COUNTER FUNCTION
+
+  function coinCounter(x) {
+
+  // Logging x (the userInput) to the console
+  console.log(x);
 
   // Initialize a JavaScript object to hold the coins
   var coinPurse = {};
-
+            
   var coinRemainder = 0;
 
-  coinPurse.quarters = 0;
-  coinPurse.dimes = 0;
-  coinPurse.nickles = 0;
-  coinPurse.pennies = 0;
+    coinPurse.quarters = 0;
+    coinPurse.dimes = 0;
+    coinPurse.nickles = 0;
+    coinPurse.pennies = 0;
 
-  coinPurse.quarters = Math.floor(x/0.25)
-  coinRemainder =  x - (coinPurse.quarters * 0.25)
 
-  coinPurse.dimes = Math.floor(coinRemainder/0.10)
-  coinRemainder = coinRemainder - (coinPurse.dimes * 0.10)
+    coinPurse.quarters = Math.floor(x/0.25)
+    coinRemainder =  x - (coinPurse.quarters * 0.25)
 
-  coinPurse.nickles = Math.floor(coinRemainder/0.05)
-  coinRemainder = coinRemainder - (coinPurse.nickles * 0.05)
+    coinPurse.dimes = Math.floor(coinRemainder/0.10)
+    coinRemainder = coinRemainder - (coinPurse.dimes * 0.10)
 
-  coinPurse.pennies = Math.floor(coinRemainder/0.01)
-  coinRemainder = coinRemainder - (coinPurse.pennies * 0.01)
+    coinPurse.nickles = Math.floor(coinRemainder/0.05)
+    coinRemainder = coinRemainder - (coinPurse.nickles * 0.05)
 
-  return coinPurse;
+    coinPurse.pennies = Math.floor(coinRemainder/0.01)
+    coinRemainder = coinRemainder - (coinPurse.pennies * 0.01)
 
-}
+    // Return results to console
+    return coinPurse;
 
-  console.log(coinCounter (0.67))
-  console.log(coinCounter (2.50))
+  }
+
+
+
+// EVENT LISTENER FOR "COUNT MY COINS" BUTTON
+ 
+  //Add event listener to calculateButton to listen for clicks
+  calculateButton.addEventListener('click', function(e) {
+
+  // Preventing default form refresh
+    e.preventDefault();
+
+    // Resetting the resultDiv to erase the inner elements for previous cases
+    resultDiv.innerHTML = "";
+
+  // console.log(userInput.value);
+
+  // coinCounter(userInput.value);
+
+  console.log(coinCounter(userInput.value));
+
+    });
+
+
+
+
+
+// 1. Write another function that builds out the html as it iterates through the object; for/in loop
+
+// 2. Call the function from line 52 instead of return
+
+// 3. Pass into the function the coinPurse
+
+
